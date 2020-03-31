acc83c534babf79d5d6b7cc604bf2ed298e82944
XandraV <veresalexandra95@gmail.com>
2020-03-31 21:34:00 +0100

    Minor change to accept array for gates property.

--- hello_template/driver.py
+++ hello_template/driver.py
@@ -4,10 +4,11 @@ import re
 from Something import ApplyGate
 
 def get_result(start, gate_names, phase_angle):
-    print(start)
-    print(re.findall('[A-Z][^A-Z]*', gate_names))
+    #print(start)
+    #print(re.findall('[A-Z][^A-Z]*', gate_names))
     result = start
-    for gate in list(gate_names):
+    print(gate_names)
+    for gate in gate_names:
         result = ApplyGate.simulate(start_state=result, gate=gate, angle=math.radians(int(phase_angle)))
     print(result)
     return result
